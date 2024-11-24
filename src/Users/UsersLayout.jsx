920ff1ec4ca89414bd207ce5c86b8ee8b960de6d
zakaria menerva <zakariamenerva@gmail.com>
2024-11-24 19:05:22 +0100

    modifie layout add package .json

--- src/Users/UsersLayout.jsx
+++ src/Users/UsersLayout.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {BrowserRouter, Link, Outlet, Route, Routes} from "react-router-dom";
 import UsersTable from "./UsersTable";
-import UsersAdd from "./UsersAdd";
-import UsersEdit from "./UsersEdit";
-import UsersDelete from "./UsersDelete";
+//import UsersAdd from "./UsersAdd";
+//import UsersEdit from "./UsersEdit";
+//import UsersDelete from "./UsersDelete";
 
 function UsersLayout() {
     return (
